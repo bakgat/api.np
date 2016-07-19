57e1c84863478de72388b583d889e079692076e6
bakgat <karl.vaniseghem@klimtoren.be>
2016-07-19 23:23:51 +0200

    initial commit

---
+++ app/Domain/Model/Education/Exceptions/MajorNotFoundException.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: karlvaniseghem
+ * Date: 24/06/16
+ * Time: 09:08
+ */
+
+namespace  App\Domain\Model\Identity\Exceptions;
+
+
+use \Exception;
+
+class MajorNotFoundException extends Exception
+{
+
+    /**
+     * MajorNotFoundException constructor.
+     * @param Uuid $id
+     */
+    public function __construct($id)
+    {
+        $msg = 'Major with id (' . $id . ') was not found.';
+        parent::__construct($msg);
+    }
+}
