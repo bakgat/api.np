1740707c47f0697cd34fa6c13d16afe0b3ba9aea
bakgat <karl.vaniseghem@klimtoren.be>
2016-07-20 12:13:49 +0200

    feature(BranchRepo): get and find tests

--- tests/Education/BranchDoctrineRepositoryTest.php
+++ tests/Education/BranchDoctrineRepositoryTest.php
@@ -2,6 +2,7 @@
 use App\Domain\Model\Education\Branch;
 use App\Domain\Model\Education\BranchRepository;
 use App\Domain\Model\Education\Exceptions\BranchNotFoundException;
+use App\Domain\Model\Education\Exceptions\MajorNotFoundException;
 use App\Domain\Model\Education\Major;
 use App\Domain\Model\Identity\GroupRepository;
 use App\Repositories\Education\BranchDoctrineRepository;
@@ -54,7 +55,6 @@ class BranchDoctrineRepositoryTest extends DoctrineTestCase
      * @test
      * @group group
      * @group branch
-     * @group major
      * @group find
      */
     public function should_find_existing_branch_by_its_id()
@@ -73,7 +73,6 @@ class BranchDoctrineRepositoryTest extends DoctrineTestCase
      * @test
      * @group group
      * @group branch
-     * @group major
      * @group find
      */
     public function should_return_null_when_no_branch_found()
@@ -87,7 +86,6 @@ class BranchDoctrineRepositoryTest extends DoctrineTestCase
      * @test
      * @group group
      * @group branch
-     * @group major
      * @group get
      */
     public function should_get_branch_by_its_id()
@@ -106,13 +104,75 @@ class BranchDoctrineRepositoryTest extends DoctrineTestCase
      * @test
      * @group group
      * @group branch
-     * @group major
      * @group get
      */
     public function should_throw_when_get_branch_fails()
     {
         $this->setExpectedException(BranchNotFoundException::class);
         $fakeId = Uuid::generate(4);
-        $branch = $this->branchRepo->getBranch($fakeId);
+        $this->branchRepo->getBranch($fakeId);
+    }
+
+    /**
+     * @test
+     * @group group
+     * @group major
+     * @group find
+     */
+    public function should_find_existing_major_by_its_id()
+    {
+        $groups = $this->groupRepo->all();
+        $branches = $this->branchRepo->all($groups[0]);
+        $id = Uuid::import($branches[0]->getMajor()->getId());
+        $this->em->clear();
+
+        $major = $this->branchRepo->findMajor($id);
+
+        $this->assertInstanceOf(Major::class, $major);
+        $this->assertEquals($major->getId(), $id);
+    }
+
+    /**
+     * @test
+     * @group group
+     * @group major
+     * @group find
+     */
+    public function should_return_null_when_no_major_is_found()
+    {
+        $fakeId = Uuid::generate(4);
+        $major = $this->branchRepo->findMajor($fakeId);
+        $this->assertNull($major);
+    }
+
+
+    /**
+     * @test
+     * @group group
+     * @group major
+     * @group get
+     */
+    public function should_get_major_by_its_id() {
+        $groups = $this->groupRepo->all();
+        $branches = $this->branchRepo->all($groups[0]);
+        $id = Uuid::import($branches[0]->getMajor()->getId());
+        $this->em->clear();
+
+        $major = $this->branchRepo->getMajor($id);
+
+        $this->assertInstanceOf(Major::class, $major);
+        $this->assertEquals($major->getId(), $id);
+    }
+
+    /**
+     * @test
+     * @group group
+     * @group major
+     * @group get
+     */
+    public function should_throw_when_get_major_fails() {
+        $this->setExpectedException(MajorNotFoundException::class);
+        $fakeId = Uuid::generate(4);
+        $this->branchRepo->getMajor($fakeId);
     }
 }
