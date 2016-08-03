df715c5e38d84d9052afe364fafa046abb42409a
bakgat <karl.vaniseghem@klimtoren.be>
2016-08-03 21:52:15 +0200

    feature(PointResult): test range 10 to 100 for average and median

--- tests/Evaluation/PointResultTest.php
+++ tests/Evaluation/PointResultTest.php
@@ -26,21 +26,23 @@ class PointResultTest extends TestCase
         $evType = new EvaluationType(EvaluationType::POINT);
         $title = $this->faker->words(3);
 
-        $fn = $this->faker->firstName();
-        $ln = $this->faker->lastName();
-        $email = $this->faker->email();
-        $gender = new Gender(Gender::MALE);
+        $evaluation = new Evaluation($branch, $evType, $title, new DateTime, 100);
 
-        $evaluation = new Evaluation($branch, $evType, $title, new DateTime, 30);
-        $student = new Student($fn, $ln, $email, $gender);
 
-        $result = new PointResult($student, 20);
-        $evaluation->addResult($result);
-        $result = new PointResult($student, 30);
-        $evaluation->addResult($result);
+        foreach (range(1, 10) as $index) {
+            $fn = $this->faker->firstName();
+            $ln = $this->faker->lastName();
+            $email = $this->faker->email();
+            $gender = new Gender(Gender::MALE);
 
-        $this->assertEquals(25, $evaluation->getAverage());
-        //$this->assertEquals(30, $evaluation->getMax());
-        $this->assertEquals(25, $evaluation->getMedian());
+            $student = new Student($fn, $ln, $email, $gender);
+            $result = new PointResult($student, $index * 10);
+            $evaluation->addResult($result);
+        }
+
+
+        $this->assertEquals(55, $evaluation->getAverage());
+        $this->assertEquals(55, $evaluation->getMedian());
+        $this->assertEquals(100, $evaluation->getMax());
     }
 }
