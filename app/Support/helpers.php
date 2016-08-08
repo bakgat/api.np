11cabbd59caf961a904c022efbcb35c61e967889
bakgat <karl.vaniseghem@klimtoren.be>
2016-08-08 14:23:32 +0200

    feature(core): collection average/median

---
+++ app/Support/helpers.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: karlvaniseghem
+ * Date: 8/08/16
+ * Time: 09:53
+ */
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+if (!function_exists('collection_average')) {
+    /**
+     * Returns average of a given field in a collection.
+     *
+     * @param Collection $collection
+     * @param string $field
+     * @return float
+     */
+    function collection_average(Collection $collection, $field)
+    {
+        $avg = 0;
+        if ($collection->count() === 0) return 0;
+
+        $collection->forAll(function ($index, $element) use (&$avg, $field) {
+            $obj = new \ReflectionObject($element);
+            $prop = $obj->getProperty($field);
+            $prop->setAccessible(true);
+
+            $avg += $prop->getValue($element);
+            return true;
+        });
+
+        return $avg / $collection->count();
+    }
+}
+
+if (!function_exists('collection_median')) {
+    /**
+     * Returns median of a given field in a collection.
+     *
+     * @param Collection $collection
+     * @param string $field
+     * @return float
+     */
+    function collection_median(Collection $collection, $field)
+    {
+        $iterator = sorted_iterator_on($collection, $field);
+
+
+        if ($iterator->count() === 0) return 0;
+        if ($iterator->count() % 2 === 0) {
+
+            $prev = $iterator[ceil($iterator->count() / 2) - 1];
+            $next = $iterator[ceil($iterator->count() / 2)];
+
+            $obj = new \ReflectionObject($prev);
+            $prop = $obj->getProperty($field);
+            $prop->setAccessible(true);
+
+
+            return ($prop->getValue($prev) + $prop->getValue($next)) / 2;
+        } else {
+            $element = $iterator[floor($iterator->count() / 2)];
+            $obj = new \ReflectionObject($element);
+            $prop = $obj->getProperty($field);
+            $prop->setAccessible(true);
+
+            return $prop->getValue($element);
+        }
+    }
+}
+
+
+if (!function_exists('sorted_iterator_on')) {
+    /**
+     * @param Collection $collection
+     * @param $field
+     * @return ArrayIterator
+     */
+    function sorted_iterator_on(Collection $collection, $field)
+    {
+        $iterator = $collection->getIterator();
+        $iterator->uasort(function ($first, $second) use ($field) {
+            $fObj = new \ReflectionObject($first);
+            $fProp = $fObj->getProperty($field);
+            $fProp->setAccessible(true);
+
+            $sObj = new \ReflectionObject($second);
+            $sProp = $sObj->getProperty($field);
+            $sProp->setAccessible(true);
+
+            $firstPrice = abs($fProp->getValue($first));
+            $secondPrice = abs($sProp->getValue($second));
+
+            if ($firstPrice == $secondPrice) return 0;
+            else return ($firstPrice < $secondPrice ? -1 : 1);
+        });
+        $list = iterator_to_array($iterator, false);
+        return new \ArrayIterator($list);
+    }
+}
