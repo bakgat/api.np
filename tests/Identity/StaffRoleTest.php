85abbc216f4e5f6ea885482f2d70d25c663a5a43
bakgat <karl.vaniseghem@klimtoren.be>
2016-08-15 09:39:11 +0200

    Test(StaffRole): should set end date

--- tests/Identity/StaffRoleTest.php
+++ tests/Identity/StaffRoleTest.php
@@ -11,6 +11,12 @@ use App\Domain\Model\Identity\Staff;
  */
 class StaffRoleTest extends TestCase
 {
+    /**
+     * @test
+     * @group staff
+     * @group role
+     * @group staffrole
+     */
     public function should_add_staff_to_role() {
         $role1 = new Role($this->faker->unique()->word());
         $group2 = new Role($this->faker->unique()->word());
@@ -29,4 +35,32 @@ class StaffRoleTest extends TestCase
         $this->assertCount(2, $staff->getRoles());
         $this->assertCount(1, $staff->getActiveRoles());
     }
+
+    /**
+     * @test
+     * @group staff
+     * @group role
+     */
+    public function should_end_active_group_for_staff()
+    {
+        $role1 = new Role($this->faker->unique()->word());
+        $role2 = new Role($this->faker->unique()->word());
+
+        $fn = $this->faker->firstName();
+        $ln = $this->faker->lastName();
+        $email = $this->faker->email();
+        $gender = new Gender($this->faker->randomElement(['F', 'M']));
+
+        $staff = new Staff($fn, $ln, $email, $gender);
+
+        $staff->assignRole($role1);
+        $this->assertCount(1, $staff->getRoles());
+
+        $now = new DateTime();
+        $staff->assignRole($role2, null, $now->modify("-1 day"));
+        $this->assertCount(2, $staff->getRoles());
+        $this->assertCount(1, $staff->getActiveRoles());
+    }
+
+
 }
