c772c9dbdfffd468637c69c4a21e1746b6d25837
bakgat <karl.vaniseghem@klimtoren.be>
2016-08-15 09:47:16 +0200

    Feature(StaffRepo): first commit

--- app/Domain/Model/Identity/StaffRepository.php
+++ app/Domain/Model/Identity/StaffRepository.php
@@ -9,6 +9,7 @@
 namespace App\Domain\Model\Identity;
 
 
+use App\Domain\Uuid;
 use Doctrine\Common\Collections\ArrayCollection;
 
 interface StaffRepository
@@ -21,11 +22,60 @@ interface StaffRepository
     public function all();
 
     /**
-     * Get all active staff members active in a role.
+     * Get all active staff members in a role.
      *
      * @param Role $group
+     * @param DateTime|null $date
      * @return ArrayCollection
      */
-    public function allInRole(Role $group);
+    public function allActiveInRole(Role $group, $date=null);
 
+    /**
+     * Gets all the active staff members in a group.
+     *
+     * @param Group $group
+     * @param DateTime|null $date
+     * @return Collection
+     */
+    public function allActiveInGroup(Group $group, $date=null);
+
+    /**
+     * Finds a staff member by its id, if not returns null.
+     *
+     * @param Uuid $id
+     * @return Staff|null
+     */
+    public function find(Uuid $id);
+
+    /**
+     * Gets an existing staff member by its id.
+     *
+     * @param Uuid $id
+     * @return Student
+     */
+    public function get(Uuid $id);
+
+    /**
+     * Saves a new staff member.
+     *
+     * @param Staff $staff
+     * @return Uuid
+     */
+    public function insert(Staff $staff);
+
+    /**
+     * Saves an existing staff member.
+     *
+     * @param Staff $staff
+     * @return int Number of affected rows.
+     */
+    public function update(Staff $staff);
+
+    /**
+     * Deletes an existing staff member.
+     *
+     * @param Uuid $id
+     * @return int Number of affected rows.
+     */
+    public function delete(Uuid $id);
 }
---
+++ app/Repositories/Identity/StaffDoctrineRepository.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: karlvaniseghem
+ * Date: 15/08/16
+ * Time: 09:44
+ */
+
+namespace App\Repositories\Identity;
+
+
+use App\Domain\Model\Identity\Collection;
+use App\Domain\Model\Identity\DateTime;
+use App\Domain\Model\Identity\Group;
+use App\Domain\Model\Identity\Role;
+use App\Domain\Model\Identity\Staff;
+use App\Domain\Model\Identity\StaffRepository;
+use App\Domain\Model\Identity\Student;
+use App\Domain\Uuid;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\EntityManager;
+
+class StaffDoctrineRepository implements StaffRepository
+{
+    /** @var  EntityManager */
+    protected $em;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * Get all staff members.
+     *
+     * @return ArrayCollection
+     */
+    public function all()
+    {
+        $qb = $this->em->createQueryBuilder();
+        $qb->select('s, sig, g, sr, r')
+            ->from(Staff::class, 's')
+            ->join('s.staffInGroups', 'sig')
+            ->join('sig.group', 'g')
+            ->leftJoin('s.staffRoles', 'sr')
+            ->join('sr.role', 'r')
+            ->orderBy('s.lastName');
+        return $qb->getQuery()->getResult();
+    }
+
+    /**
+     * Get all active staff members in a role.
+     *
+     * @param Role $group
+     * @param DateTime|null $date
+     * @return ArrayCollection
+     */
+    public function allActiveInRole(Role $group, $date = null)
+    {
+        // TODO: Implement allActiveInRole() method.
+    }
+
+    /**
+     * Gets all the active staff members in a group.
+     *
+     * @param Group $group
+     * @param DateTime|null $date
+     * @return Collection
+     */
+    public function allActiveInGroup(Group $group, $date = null)
+    {
+        // TODO: Implement allActiveInGroup() method.
+    }
+
+    /**
+     * Finds a staff member by its id, if not returns null.
+     *
+     * @param Uuid $id
+     * @return Staff|null
+     */
+    public function find(Uuid $id)
+    {
+        // TODO: Implement find() method.
+    }
+
+    /**
+     * Gets an existing staff member by its id.
+     *
+     * @param Uuid $id
+     * @return Student
+     */
+    public function get(Uuid $id)
+    {
+        // TODO: Implement get() method.
+    }
+
+    /**
+     * Saves a new staff member.
+     *
+     * @param Staff $staff
+     * @return Uuid
+     */
+    public function insert(Staff $staff)
+    {
+        // TODO: Implement insert() method.
+    }
+
+    /**
+     * Saves an existing staff member.
+     *
+     * @param Staff $staff
+     * @return int Number of affected rows.
+     */
+    public function update(Staff $staff)
+    {
+        // TODO: Implement update() method.
+    }
+
+    /**
+     * Deletes an existing staff member.
+     *
+     * @param Uuid $id
+     * @return int Number of affected rows.
+     */
+    public function delete(Uuid $id)
+    {
+        // TODO: Implement delete() method.
+    }
+}
