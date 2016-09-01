2848cad9478dd672e8f1866ee68b0e8555a0f13a
bakgat <karl.vaniseghem@klimtoren.be>
2016-09-01 21:59:18 +0200

    feature(Uuid): test coverage

---
+++ tests/UuidTest.php
@@ -0,0 +1,38 @@
+<?php
+use App\Domain\Model\Identity\Group;
+use App\Domain\Uuid;
+
+/**
+ * Created by PhpStorm.
+ * User: karlvaniseghem
+ * Date: 1/09/16
+ * Time: 21:45
+ */
+class UuidTest extends TestCase
+{
+    /**
+     * @test
+     * @group uuid
+     */
+    public function should_create_new() {
+        $uuid = Uuid::generate(4);
+
+        $this->assertInstanceOf(Uuid::class, $uuid);
+        $this->assertCount(5, explode('-', (string)$uuid));
+    }
+
+    /**
+     * @test
+     * @group uuid
+     */
+    public function should_serialize_to_json() {
+        $serializer = \JMS\Serializer\SerializerBuilder::create()->build();
+
+        $group = new Group($this->faker->word);
+
+        $serialized = $serializer->serialize($group, 'json');
+        $expected = '"id":"' . (string)$group->getId() . '"';
+
+        $this->assertContains($expected, $serialized);
+    }
+}
