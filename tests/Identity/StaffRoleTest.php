9dfee6ce3897a1c67724572d8947d04cb1a0ea7a
bakgat <karl.vaniseghem@klimtoren.be>
2016-09-01 22:09:40 +0200

    Update(StaffRole): test all staff roles

--- tests/Identity/StaffRoleTest.php
+++ tests/Identity/StaffRoleTest.php
@@ -2,6 +2,7 @@
 use App\Domain\Model\Identity\Gender;
 use App\Domain\Model\Identity\Role;
 use App\Domain\Model\Identity\Staff;
+use App\Domain\Model\Identity\StaffRole;
 
 /**
  * Created by PhpStorm.
@@ -17,7 +18,8 @@ class StaffRoleTest extends TestCase
      * @group role
      * @group staffrole
      */
-    public function should_add_staff_to_role() {
+    public function should_add_staff_to_role()
+    {
         $role1 = new Role($this->faker->unique()->word());
         $group2 = new Role($this->faker->unique()->word());
 
@@ -37,10 +39,10 @@ class StaffRoleTest extends TestCase
     }
 
     /**
-     * @test
-     * @group staff
-     * @group role
-     */
+ * @test
+ * @group staff
+ * @group role
+ */
     public function should_end_active_group_for_staff()
     {
         $role1 = new Role($this->faker->unique()->word());
@@ -62,5 +64,30 @@ class StaffRoleTest extends TestCase
         $this->assertCount(1, $staff->getActiveRoles());
     }
 
+    /**
+     * @test
+     * @group staff
+     * @group role
+     */
+    public function should_get_all_staff_roles()
+    {
+        $role1 = new Role($this->faker->unique()->word());
+        $role2 = new Role($this->faker->unique()->word());
 
+        $fn = $this->faker->firstName();
+        $ln = $this->faker->lastName();
+        $email = $this->faker->email();
+        $gender = new Gender($this->faker->randomElement(['F', 'M']));
+
+        $staff = new Staff($fn, $ln, $email, $gender);
+
+        $staff->assignRole($role1);
+
+        $now = new DateTime();
+        $staff->assignRole($role2, null, $now->modify("-1 day"));
+
+
+        $this->assertCount(2, $staff->allStaffRoles());
+        $this->assertInstanceOf(StaffRole::class, $staff->allStaffRoles()[0]);
+    }
 }
