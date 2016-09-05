3dd0793dd121407472fd659a2d67247f8a4a5f8b
bakgat <karl.vaniseghem@klimtoren.be>
2016-09-05 16:32:29 +0200

    Feature(StudentController): first test

--- tests/Controllers/Identity/StudentControllerTest.php
+++ tests/Controllers/Identity/StudentControllerTest.php
@@ -1,4 +1,9 @@
 <?php
+use App\Domain\Model\Identity\Gender;
+use App\Domain\Model\Identity\Student;
+use App\Domain\Services\Identity\StudentService;
+use Doctrine\Common\Collections\ArrayCollection;
+use Mockery\MockInterface;
 
 /**
  * Created by PhpStorm.
@@ -8,5 +13,66 @@
  */
 class StudentControllerTest extends TestCase
 {
+    /** @var MockInterface */
+    protected $groupRepo;
+    /** @var MockInterface */
+    protected $studentRepo;
+    /** @var StudentService */
+    protected $studentService;
 
+    public function setUp()
+    {
+        parent::setUp();
+        $this->studentRepo = $this->mock(App\Domain\Model\Identity\StudentRepository::class);
+        $this->groupRepo = $this->mock(App\Domain\Model\Identity\GroupRepository::class);
+    }
+
+    /**
+     * @test
+     * @group StudentController
+     */
+    public function should_serialize_index()
+    {
+        $this->studentRepo->shouldReceive('all')
+            ->once()
+            ->andReturn($this->makeStudentCollection());
+
+        $this->get('students')
+            ->seeJsonStructure([
+                '*' => [
+                    'id',
+                    'displayName',
+                    'firstName',
+                    'lastName',
+                    'gender',
+                    'birthday',
+                    'activeGroups'
+                ]
+            ]);
+    }
+
+    /*
+    * PRIVATE METHODS
+    */
+    private function makeStudentCollection()
+    {
+        $collection = new ArrayCollection();
+        foreach (range(1, 10) as $item) {
+            $student = $this->makeStudent();
+            $collection->add($student);
+        }
+        return $collection;
+    }
+
+    private function makeStudent()
+    {
+        $fn = $this->faker->firstName;
+        $ln = $this->faker->lastName;
+        $schoolId = $this->faker->bankAccountNumber;
+        $birthday = $this->faker->dateTime;
+        $gender = $this->faker->randomElement(Gender::values());
+
+        $student = new Student($fn, $ln, $schoolId, $gender, $birthday);
+        return $student;
+    }
 }
