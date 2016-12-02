be01154c19e25a5c52cf133a8ded0faed65a7b7b
bakgat <karl.vaniseghem@klimtoren.be>
2016-12-02 09:33:08 +0100

    Fix(helpers): resource path for lumen

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -111,3 +111,16 @@ if (!function_exists('convert_date_from_string')) {
         return $date;
     }
 }
+
+if (! function_exists('resource_path')) {
+    /**
+     * Get the path to the resources folder.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    function resource_path($path = '')
+    {
+        return app()->basePath().DIRECTORY_SEPARATOR.'resources'.($path ? DIRECTORY_SEPARATOR.$path : $path);
+    }
+}
