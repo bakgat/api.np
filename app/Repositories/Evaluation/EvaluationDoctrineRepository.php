bf5815e7db11637a82f616d63dbb6866bf1ce03d
bakgat <karl.vaniseghem@klimtoren.be>
2016-12-06 16:26:28 +0100

    Fix(EvaluationRepo): in range sql statement

--- app/Repositories/Evaluation/EvaluationDoctrineRepository.php
+++ app/Repositories/Evaluation/EvaluationDoctrineRepository.php
@@ -319,18 +319,23 @@ class EvaluationDoctrineRepository implements EvaluationRepository
                 GROUP BY s.id
                 ORDER BY sig.number";
         return $this->getFeedbackReport($sql);
+
+
     }
 
     public function getRedicodiReportForGroup($group, DateRange $range)
     {
+        $rs = $range->getStart()->format('Y-m-d');
+        $re = $range->getEnd()->format('Y-m-d');
+
+        $inRangeSql = "(rfs.start <= '{$rs}' AND rfs.end >= '{$re}') OR ('{$rs}' > rfs.start AND rfs.end <= '{$re}') OR ('{$rs}' < rfs.start AND rfs.end >= '{$re}')";
+
         $sql = "SELECT s.id as s_id, s.first_name as first_name, s.last_name as last_name,
                 rfs.redicodi as rfs_redicodi
                 FROM redicodi_for_students rfs
                 INNER JOIN students s ON s.id = rfs.student_id
                 INNER JOIN student_in_groups sig on sig.student_id = s.id
-                WHERE rfs.start <= '" . $range->getStart()->format('Y-m-d') . "' 
-                    AND rfs.end >='" . $range->getEnd()->format('Y-m-d') . "'
-                    AND sig.group_id='" . $group . "'
+                WHERE {$inRangeSql}
                 GROUP BY s.id, rfs.redicodi
                 ORDER BY sig.number";
 
